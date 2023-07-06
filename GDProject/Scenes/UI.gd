884a8eae3dfdeba601e53c1bdfe41b9a3db135a1
AsperTheDog <guillerman0000@gmail.com>
2023-07-06 03:29:14 +0200

    Thinking bubble now works

--- GDProject/Scenes/UI.gd
+++ GDProject/Scenes/UI.gd
@@ -1,8 +1,16 @@
 extends Control
 
+signal bubbleShown
+signal bubbleHidden
+
 @export_group("That's all folks")
 @export var thatsAllSpeed: float = 1
 @export var thatsAllCurve: Curve
+@export_group("Thinking bubble")
+@export var bubbleAppearSpeed: float = 1
+@export var bubbleDissappearSpeed: float = 1
+@export var bubbleXCurve: Curve
+@export var bubbleYCurve: Curve
 
 var thatsAllExecuting: bool = true
 var thatsAllFadeOut: bool = true
@@ -10,6 +18,8 @@ var thatsAllStep: float = 0
 
 var showDebug: bool = false
 var chara: MainCharacter
+
+var bubbleInProcess: bool = false
 
 func _ready():
 	doThatsAllFolks(true)
@@ -43,9 +53,43 @@ func doThatsAllFolks(isFadeOut: bool):
 	thatsAllStep = int(not isFadeOut)
 	thatsAllFadeOut = isFadeOut
 	thatsAllExecuting = true
-	
+
+func changeBubbleScale(bubble: Sprite2D, appear: bool):
+	var counter = 0 if appear else 1
+	var speed = bubbleAppearSpeed if appear else bubbleDissappearSpeed
+	while (counter < 1 and appear) or (counter > 0 and not appear):
+		bubble.scale = Vector2(bubbleXCurve.sample(counter), bubbleYCurve.sample(counter))
+		counter += get_process_delta_time() * (int(appear) * 2 - 1) * speed
+		await get_tree().process_frame
+	var objective = 1 if appear else 0
+	bubble.scale = Vector2(objective, objective)
+
 func showBubble():
-	print("Showing bubble")
+	if bubbleInProcess:
+		await bubbleHidden
+	bubbleInProcess = true
+	$"InfoLevel Container/small".scale = Vector2(0, 0)
+	$"InfoLevel Container/medium".scale = Vector2(0, 0)
+	$"InfoLevel Container/big".scale = Vector2(0, 0)
+	$"InfoLevel Container".show()
+	changeBubbleScale($"InfoLevel Container/small", true)
+	await get_tree().create_timer(1 / bubbleAppearSpeed).timeout
+	changeBubbleScale($"InfoLevel Container/medium", true)
+	await get_tree().create_timer(1 / bubbleAppearSpeed).timeout
+	changeBubbleScale($"InfoLevel Container/big", true)
+	bubbleInProcess = false
+	bubbleShown.emit()
 
 func hideBubble():
-	print("Hiding bubble")
+	if bubbleInProcess:
+		await bubbleShown
+	bubbleInProcess = true
+	changeBubbleScale($"InfoLevel Container/big", false)
+	await get_tree().create_timer(1 / bubbleDissappearSpeed).timeout
+	changeBubbleScale($"InfoLevel Container/medium", false)
+	await get_tree().create_timer(1 / bubbleDissappearSpeed).timeout
+	changeBubbleScale($"InfoLevel Container/small", false)
+	await get_tree().create_timer(1 / bubbleDissappearSpeed).timeout
+	$"InfoLevel Container".hide()
+	bubbleInProcess = false
+	bubbleHidden.emit()
