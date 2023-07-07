ccbef02335a29db96e949ec4e31511bd66b493ad
AsperTheDog <guillerman0000@gmail.com>
2023-07-07 15:36:46 +0200

    Arreglado el fadeIn del menu

--- GDProject/Scenes/Game-Menu.gd
+++ GDProject/Scenes/Game-Menu.gd
@@ -15,12 +15,11 @@ func _ready():
 
 func fadeInOut(fadeIn: bool):
 	var fade = $"FadeInLayer/ColorRect"
-	var count = 0 if fadeIn else 1
-	fade.color = Color(0, 0, 0, 1 if fadeIn else 0)
-	while (count < 1 and fadeIn) or (count > 0 and not fadeIn):
-		fade.color = Color(0, 0, 0, lerp(1, 0, count))
+	var count = fade.color.a
+	while (count < 1 and not fadeIn) or (count > 0 and fadeIn):
+		fade.color = Color(0, 0, 0, lerp(0, 1, count))
 		var speed = fadeInSpeed if fadeIn else fadeOutSpeed
-		count += get_process_delta_time() * fadeInSpeed * (int(fadeIn) * 2 - 1)
+		count += get_process_delta_time() * speed * (int(not fadeIn) * 2 - 1)
 		await get_tree().process_frame
 	fade.color = Color(0, 0, 0, 0 if fadeIn else 1)
 	fadeOutFinished.emit()
